920197fd3c8cc9cbc4908afa158d08e54305ed19
Josue Jimenez <josuepjimez@gmail.com>
2022-08-30 08:25:07 -0500

    Ideas de botones

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { CustomButtonNavigate } from '../shared/atoms/CustomButtonNavigate';
 // import { CustomButtonNavigate } from '../shared/atoms/CustomButtonNavigate';
 
 export function Home() {
     return (
         <>
             <div className=".container-md row" style={{ backgroundColor: '#B9A6E0', margin: '0px 10px 0px 10px', padding: '20px' }}>
-                <div className='col-lg-3 col-sm-1'>Mas Vendidos</div>
-                <div className='col-lg-3 col-sm-1'>Hombre </div>
-                <div className='col-lg-3 col-sm-1'>Mujer</div>
+                <div className='col-lg-3 col-sm-1'><CustomButtonNavigate label='Mas Vendido' /></div>
+                <div className='col-lg-3 col-sm-1'><CustomButtonNavigate label='Hombre' /> </div>
+                <div className='col-lg-3 col-sm-1'><CustomButtonNavigate label='Mujer' /></div>
                 <div className='col-lg-3 col-sm-1 row'>
                     <form className="d-flex" style={{ margin: '0px 8%' }}>
                         <input className="form-control me-4" type="search" placeholder="¿Que estas buscando?..." />
