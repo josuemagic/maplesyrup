f95c713b103863969ee4da9b8a376b89e8303a70
JosueJimenezDeLucio <jjimenez@siccob.com.mx>
2022-09-05 15:11:39 -0500

    Correccion de arreglo en el Merge

--- frontend/src/components/shared/atoms/CustomButtonNavigate.jsx
+++ frontend/src/components/shared/atoms/CustomButtonNavigate.jsx
@@ -7,14 +7,8 @@ export function CustomButtonNavigate({ label = "", linkTo = "home" }) {
     return (
         <>
             <div className="middle">
-<<<<<<< HEAD
-                <Link className="nav-link btnCustom btn1" to={linkTo}><strong className=''>{label}</strong></Link>
-                {/* <Link className="nav-link" to={linkTo}><strong style={{ fontSize: '20px', margin: '20px' }}>{label}</strong></Link> */}
-
-=======
                 <a href="" className="btnCustom btn1">{label}</a>
                 {/* <a href="" class="btn btn3">Hover Me</a> */}
->>>>>>> 73ca631438907855e8bc47e85190e4f3eaaf789f
             </div>
         </>
     )
